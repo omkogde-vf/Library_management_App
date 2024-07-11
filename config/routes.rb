ee061057fa11d2fb7d499a52c00c7686fe917d1f
Om Kogde <okogde@verticalfox.com>
2024-07-11 18:46:56 +0530

    updated route for json request

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 # config/routes.rb
 Rails.application.routes.draw do
-  devise_for :users
-
-  authenticated :user do
-    root 'pages#home', as: :authenticated_root
-  end
-
-  root to: 'pages#welcome'
-  get 'about', to: 'pages#about'
-  resources :books
+  resources :books, defaults: {format: :json}
 end
