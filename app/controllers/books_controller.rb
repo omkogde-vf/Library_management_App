ac5fdb83f0b598c3668153faf7cd23eb9a250ef0
Om Kogde <okogde@verticalfox.com>
2024-07-15 00:20:32 +0530

    Modified books controller to respond in json format to http req from react

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,61 +1,48 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_book, only: %i[ show edit update destroy ]
+  before_action :set_book, only: %i[show edit update destroy]
   
   # GET /books or /books.json
   def index
     @books = Book.all
+    render json: @books
   end
 
   # GET /books/1 or /books/1.json
   def show
+    render json: @book
   end
 
   # GET /books/new
   def new
     @book = Book.new
-  end
-
-  # GET /books/1/edit
-  def edit
+    render json: @book
   end
 
   # POST /books or /books.json
   def create
     @book = Book.new(book_params)
 
-    respond_to do |format|
-      if @book.save
-        format.html { redirect_to book_url(@book), notice: "Book was successfully created." }
-        
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        
-      end
+    if @book.save
+      render json: @book, status: :created, location: @book
+    else
+      render json: @book.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /books/1 or /books/1.json
   def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to book_url(@book), notice: "Book was successfully updated." }
-       
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        
-      end
+    if @book.update(book_params)
+      render json: @book
+    else
+      render json: @book.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /books/1 or /books/1.json
   def destroy
     @book.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
-     
-    end
+    head :no_content
   end
 
   private
