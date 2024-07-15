5f9e6af5a87d72d4ad6b08a467960ed803ef6461
Om Kogde <okogde@verticalfox.com>
2024-07-15 22:59:31 +0530

    updated frontend code

--- app/controllers/application_controller.rb
+++
@@ -1,6 +0,0 @@
-# app/controllers/application_controller.rb
-
-class ApplicationController < ActionController::API
-end
-
-  
--- app/models/book.rb
+++
@@ -1,7 +0,0 @@
-class Book < ApplicationRecord
-    validates :title, presence: true
-    validates :author, presence: true
-    validates :description, presence: true
-    validates :published_at, presence: true
-  end
-  
