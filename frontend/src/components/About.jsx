b734dcbb44ffae20f75d332fec0d7106f68c0b05
Om Kogde <okogde@verticalfox.com>
2024-07-22 12:55:21 +0530

    Abo ut page updated

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,12 +1,24 @@
 // components/About.js
+import React from 'react';
+import './About.css';
+
 const About = () => {
-    return (
-      <div>
-        <h1>About This App</h1>
-        <p>This app allows users to manage their book collection by adding, viewing, and listing books.</p>
-      </div>
-    );
-  };
-  
-  export default About;
-  
+  return (
+    <div className="about-container">
+      <h1>About Page</h1>
+      <h2>Welcome to Library Management</h2>
+      <p>
+        Here at Sarasvati Library, we are dedicated to providing an efficient and user-friendly platform to manage our library resources effectively. Whether you're a student, faculty member, or a library staff member, our system is designed to enhance your library experience.
+      </p>
+      <h2>Our Mission</h2>
+      <p>At Sarasvati Library, our mission is to:</p>
+      <ul>
+        <li>Facilitate easy access to a diverse collection of books and resources.</li>
+        <li>Ensure seamless borrowing and returning of materials.</li>
+        <li>Promote a conducive learning environment for our patrons.</li>
+      </ul>
+    </div>
+  );
+};
+
+export default About;
