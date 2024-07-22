92c32ca98bf5a05bd7b246c844429813268f3a49
Om Kogde <okogde@verticalfox.com>
2024-07-22 13:06:29 +0530

    Updated Home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,13 @@
 // components/Home.js
+import React from 'react';
 import { Link } from 'react-router-dom';
+import './Home.css';
 
 const Home = () => {
   return (
-    <div>
+    <div className="home-container">
       <h1>Welcome to the Book Management System</h1>
       <Link to="/new">Add Book</Link>
-      <br />
       <Link to="/books">See Books Listing</Link>
     </div>
   );
