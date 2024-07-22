467d4f0142529d2522a1cf9d7ed689948aac13e1
Om Kogde <okogde@verticalfox.com>
2024-07-22 13:11:07 +0530

    included toast message and after signup user to jump on login page

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
+import {useNavigate} from 'react-router-dom'
+import { toast } from 'react-toastify';
+import './Auth.css';
 
-const Signup = ({ setCurrUser, setShow }) => {
+const Signup = ({ setCurrUser ,setShow}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const navigate = useNavigate();
 
   const handleSignup = async (e) => {
     // e.preventDefault();
@@ -25,15 +29,27 @@ const Signup = ({ setCurrUser, setShow }) => {
       if (!response.ok) throw new Error('Failed to sign up');
       const data = await response.json();
       localStorage.setItem('token', data.jwt); // assuming your backend responds with a JWT token
+      toast.success('Successfully signed up!')
       setCurrUser(data.user);
-      setShow(true); // Switch to login screen after signup
+      navigate("/login"); // Navigate to login page after signup
+       setShow(true); // Switch to login screen after signup
     } catch (error) {
+      toast.error('Failed to sign up')
       console.error(error);
     }
   };
 
+  const handleClick = (e) => {
+    e.preventDefault();
+    navigate("/login");
+  };
+
   return (
-    <form onSubmit={handleSignup}>
+    <div className='auth-container'>
+       <div className="auth-header">
+        <h1>Welcome to My Library</h1>
+      </div>
+    <form className='auth-form' onSubmit={handleSignup}>
       <div>
         <label>Email</label>
         <input
@@ -61,8 +77,12 @@ const Signup = ({ setCurrUser, setShow }) => {
           required
         />
       </div>
-      <button type="submit">Sign Up</button>
+      <button  type="submit">Sign Up</button>
+      <br />
+        Already registered, <a href="#login" onClick={handleClick}>Login</a> here.
     </form>
+    </div>
+
   );
 };
 
