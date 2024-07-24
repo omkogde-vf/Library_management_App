2f8cbbe369b3b1bd3717b775b08de980670ac039
Om Kogde <okogde@verticalfox.com>
2024-07-24 10:08:23 +0530

    Updated Home Page

--- frontend/src/components/frontpage/fj/Home.jsx
+++ frontend/src/components/frontpage/fj/Home.jsx
@@ -2,10 +2,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../fc/Home.css';
+import backgroundImage from '../../../background.jpg';
 
 const Home = () => {
   return (
-    <div className="home-container">
+    <div className="home-container" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <h1>Welcome to the Book Management System</h1>
       <Link to="/new">Add Book</Link>
       <Link to="/books">See Books Listing</Link>
